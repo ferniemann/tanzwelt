17bd0ca76514e5e34bbf740bbd3396463f39be7a
Ferdinand <hello@ferdi.digital>
2025-01-12 22:34:57 +0100

    add keyframes and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,48 @@ export default {
     "./node_modules/flyonui/flyonui.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blur: "blur 1s linear",
+        moveInLeft: "moveInLeft 1s linear",
+        moveInRight: "moveInRight 1s linear",
+        fadeIn: "fadeIn 1s linear",
+      },
+      keyframes: {
+        blur: {
+          "0%": {
+            filter: "blur(0px)",
+          },
+          "100%": {
+            filter: "blur(5px)",
+          },
+        },
+        moveInLeft: {
+          "0%": {
+            left: "100%",
+          },
+          "100%": {
+            left: "0%",
+          },
+        },
+        moveInRight: {
+          "0%": {
+            right: "100%",
+          },
+          "100%": {
+            right: "0%",
+          },
+        },
+        fadeIn: {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+      },
+    },
   },
   plugins: [require("flyonui"), require("flyonui/plugin")],
   flyonui: {
