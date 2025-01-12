18c4c8405253c8c21bb2349c00d26c02add0d38e
Ferdinand <hello@ferdi.digital>
2025-01-12 22:34:05 +0100

    max-width for website

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-screen-[1920px] mx-auto bg-sky-100/50">
+  <div class="max-w-[1920px] mx-auto bg-sky-100/50">
     <LayoutMainHeader />
     <main class="my-8 px-6 md:px-8 lg:px-20 ">
       <NuxtPage />
