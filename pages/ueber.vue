fb97b8f361dd905064350b76df0f180873bd1790
Ferdinand <hello@ferdi.digital>
2025-01-19 15:31:26 +0100

    chore: add about page

---
+++ pages/ueber.vue
@@ -0,0 +1,68 @@
+<template>
+  <article>
+    <header class="relative">
+      <h2 class="font-display text-4xl md:text-6xl text-sky-70 my-8">Ludwigs Tanzwelt
+      </h2>
+      <img src="https://fuenf-gewinnt.de/wp-content/uploads/2023/05/Tanzwelt-header1.png"
+        alt="Ludwig mit Tanzpartnerin">
+    </header>
+    <article class="content grid md:grid-cols-[1fr_3fr] my-12 md:gap-8">
+      <img src="https://fuenf-gewinnt.de/wp-content/uploads/2023/07/Tanzwelt-ohne-handicap-683x1024.png"
+        alt="Ludwig mit Tanzpartnerin"
+        class="w-48 aspect-square object-cover object-top rounded-full mx-auto md:w-auto md:aspect-auto md:rounded">
+      <article>
+
+        <h3 class="my-8 font-bold text-xl">Ludwig und sein Weg</h3>
+        <p>Als Jugendlicher hat Ludwig seine Leidenschaft fürs Tanzen entdeckt. Was als Hobby begann, wurde schnell zu
+          einer großen Liebe, die sein Leben bis heute prägt. Mit viel Ehrgeiz und Herzblut stieg er beim TSC Astoria
+          durch die Reihen der Tänzer*innen auf – und das mit beeindruckendem Erfolg. Standard und Latein wurden zu
+          seinem
+          Metier, und bald tanzte er sich von Turniersieg zu Turniersieg bis in die höchste deutsche Amateurklasse, die
+          S-Klasse. Doch für Ludwig war Tanzen nie nur Technik und Disziplin – es war immer auch eine Bühne für echte
+          Leidenschaft.</p>
+        <p>
+          Diesen Funken wollte er weitergeben. Also machte er eine Ausbildung zum Tanzlehrer und entdeckte seine zweite
+          Berufung: anderen die Freude am Tanzen zu vermitteln. Mit seiner unverwechselbaren Mischung aus Humor,
+          Empathie
+          und einem unermüdlichen Enthusiasmus motiviert er seitdem Menschen jeden Alters, die ersten Schritte zu wagen
+          oder das Beste aus sich herauszuholen.
+        </p>
+      </article>
+    </article>
+    <article>
+      <p>Doch das Leben hatte noch eine ganz andere Herausforderung für ihn vorgesehen. 2012 veränderte ein schwerer
+        Autounfall alles. Die Ärzte malten düstere Prognosen – Ludwig jedoch malte seine eigene Zukunft. Mit
+        unglaublicher Willenskraft, viel Geduld und einem festen Glauben daran, dass das Leben immer weiter tanzt,
+        kämpfte er sich nicht nur zurück auf die Beine, sondern auch zurück aufs Parkett.</p>
+    </article>
+    <article class="grid md:grid-cols-[3fr_1fr] my-12 md:gap-8">
+      <article>
+
+        <p>
+          Heute ist Ludwig mehr als ein Tänzer. Er ist ein Vorbild dafür, dass Leidenschaft und Durchhaltevermögen Berge
+          versetzen können. Mit "Ludwigs Tanzwelt", seiner eigenen Tanzschule im Herzen Lübecks, hat er einen Ort
+          geschaffen, an dem Menschen nicht nur das Tanzen lernen, sondern auch Lebensfreude tanken können.
+        </p>
+        <p>
+          Ob blutiger Anfänger oder erfahrener Tänzer, ob jung oder alt – Ludwig heißt jeden willkommen, der sich auf
+          die
+          Reise begeben will. Denn für ihn ist Tanzen mehr als ein Hobby: Es ist ein Ausdruck von Lebensfreude,
+          Gemeinschaft und persönlichem Wachstum. Und genau das möchte er mit so vielen Menschen wie möglich teilen.
+        </p>
+      </article>
+      <img src="https://fuenf-gewinnt.de/wp-content/uploads/2023/05/Tanzenhandicap-Jana.jpeg"
+        alt="Ludwig mit Tanzpartnerin"
+        class="w-48 aspect-square object-cover object-top rounded-full mx-auto md:w-auto md:aspect-auto md:rounded">
+    </article>
+    <article>
+      <p class="text-xl md:text-4xl italic">Komm vorbei und erleb, was es heißt, mit Ludwig zu tanzen: "Jeder
+        Schritt ist ein Schritt Richtung Freude!"</p>
+    </article>
+  </article>
+</template>
+
+<style scoped>
+p {
+  @apply my-4 leading-8
+}
+</style>
