5834358775ab01e1c46fbcf3179ac8d3d06c1ab5
Ferdinand <hello@ferdi.digital>
2025-05-25 11:15:42 +0200

    feat: remove dance display from CourseHeader component

--- components/layout/CourseHeader.vue
+++ components/layout/CourseHeader.vue
@@ -1,11 +1,5 @@
 <template>
   <h2 class="font-bold text-rose-900 text-3xl lg:text-4xl mb-4 font-display">{{ title }}</h2>
-  <div class="flex flex-wrap gap-2 my-4 bg-white p-4 rounded md:my-8">
-    <span class="whitespace-nowrap rounded-full bg-sky-200/50 px-2.5 py-0.5 text-sm text-sky-700"
-      v-for="dance in dances" :key="dance">
-      {{ dance }}
-    </span>
-  </div>
 </template>
 
 <script setup>
