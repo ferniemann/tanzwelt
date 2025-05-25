0123a306aa1d0d17af5ae0a713924b457d6c923e
Ferdinand <hello@ferdi.digital>
2025-05-25 11:15:46 +0200

    feat: add Impressum page with legal information and contact details

---
+++ pages/impressum.vue
@@ -0,0 +1,57 @@
+<template>
+  <article>
+    <header class="relative">
+      <h2 class="font-display text-4xl md:text-6xl text-sky-70 my-8">Impressum</h2>
+    </header>
+    <article>
+      <h3 class="font-bold text-lg">Kontaktadresse</h3>
+      <address class="flex flex-col">
+        Ludwig Köppen
+        Seekenkamp 9
+        23909 Ratzeburg
+        Deutschland
+        ludwig@pentaludea.de
+      </address>
+      <br>
+      <h3 class="font-bold text-lg">Handelsregistereintrag</h3>
+      <p>Eingetragener Firmenname: Ludwig Köppen</p>
+      <p>Handelsregisteramt: Ratzenburg</p>
+      <h3 class="font-bold text-lg">Haftungsausschluss</h3>
+      <p>
+        Der Autor übernimmt keinerlei Gewähr hinsichtlich der inhaltlichen Richtigkeit, Genauigkeit, Aktualität,
+        Zuverlässigkeit und Vollständigkeit der Informationen.
+      </p>
+      <p>
+        Haftungsansprüche gegen den Autor wegen Schäden materieller oder immaterieller Art, welche aus dem Zugriff oder
+        der Nutzung bzw. Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch
+        technische Störungen entstanden sind, werden ausgeschlossen.
+      </p>
+
+      <p>
+        Alle Angebote sind unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte
+        Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise
+        oder
+        endgültig einzustellen.
+      </p>
+      <h3 class="font-bold text-lg">Haftung für Links</h3>
+      <p>
+        Verweise und Links auf Webseiten Dritter liegen ausserhalb unseres Verantwortungsbereichs Es wird jegliche
+        Verantwortung für solche Webseiten abgelehnt. Der Zugriff und die Nutzung solcher Webseiten erfolgen auf eigene
+        Gefahr des Nutzers oder der Nutzerin.
+      </p>
+      <h3 class="font-bold text-lg">Urheberrechte</h3>
+      <p>
+        Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf der Website gehören
+        ausschliesslich der Firma Ludwig Köppen oder den speziell genannten Rechtsinhabern. Für die Reproduktion
+        jeglicher
+        Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.
+      </p>
+    </article>
+  </article>
+</template>
+
+<style scoped>
+p {
+  @apply my-4 leading-8
+}
+</style>
