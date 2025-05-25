5c4286950c7fa56985b2899ba2cd050f3b24aa1d
Ferdinand <hello@ferdi.digital>
2025-05-25 11:14:50 +0200

    feat: add additional Nuxt modules for enhanced functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   css: ["~/assets/css/global.css"],
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/fonts",
+    "@pinia/nuxt",
+    "@nuxt/image",
+  ],
   fonts: {
     provider: "bunny",
   },
